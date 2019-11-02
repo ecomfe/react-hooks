53f2f7f791d780a3100246aaeeb3a4c08d927f2a
otakustay <otakustay@gmail.com>
2019-11-02 21:07:17 +0800

    feat: input value hooks

---
+++ packages/input-value/src/__tests__/index.test.js
@@ -0,0 +1,26 @@
+import {renderHook, act} from '@testing-library/react-hooks';
+import useInputValue from '../index';
+
+test('has value and onChange', () => {
+    const {result} = renderHook(() => useInputValue());
+    expect(typeof result.current.value).toBe('string');
+    expect(typeof result.current.onChange).toBe('function');
+});
+
+test('default initial value to empty string', () => {
+    const {result} = renderHook(() => useInputValue());
+    expect(result.current.value).toBe('');
+});
+
+test('specified initial value', () => {
+    const initialValue = 'foo';
+    const {result} = renderHook(() => useInputValue(initialValue));
+    expect(result.current.value).toBe(initialValue);
+});
+
+test('change value via onChange', () => {
+    const {result} = renderHook(() => useInputValue());
+    const newValue = 'bar';
+    act(() => result.current.onChange({target: {value: newValue}}));
+    expect(result.current.value).toBe(newValue);
+});
---
+++ packages/input-value/src/index.ts
@@ -0,0 +1,15 @@
+import {useState, useCallback, ChangeEvent} from 'react';
+
+export interface InputValueState {
+    value: string;
+    onChange(e: ChangeEvent<HTMLInputElement>): void;
+}
+
+export default (initialValue: string = ''): InputValueState => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value),
+        []
+    );
+    return {value, onChange};
+};

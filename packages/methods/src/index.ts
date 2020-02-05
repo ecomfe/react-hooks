9006c59bd9d1cadb3d0718e7f4709e3d3063cc12
otakustay <otakustay@gmail.com>
2020-02-05 13:47:08 +0800

    fix: optimize type definition of useMethods

--- packages/methods/src/index.ts
+++ packages/methods/src/index.ts
@@ -1,26 +1,22 @@
 import {useMemo} from 'react';
 import {useImmerState} from '@huse/immer';
 
-export interface Methods<T> {
-    [key: string]: (current: T, ...args: any[]) => T | void;
-}
+type Reducer<S, M> = (state: S, ...args: any[]) => S | void;
 
-export type BoundMethods<M> = {[K in keyof M]: (...args: any[]) => void};
+type Reducers<S, M> = {[K in keyof M]: Reducer<S, M>};
 
-export type MethodsState<S, M> = [S, BoundMethods<M>];
-
-export function useMethods<S = any, M = Methods<S>>(methods: M, initialState: S): MethodsState<S, M> {
+export function useMethods<S, M>(methods: Reducers<S, M>, initialState: S): [S, M] {
     const [state, setState] = useImmerState(initialState);
     const boundMethods = useMemo(
         () => Object.keys(methods).reduce(
             (boundMethods, name) => {
-                const fn = methods[name] as (state: S, ...args: any[]) => S | void;
+                const fn = methods[name] as Reducer<S, M>;
                 boundMethods[name] = (...args: any[]) => setState((state: S) => fn(state, ...args));
                 return boundMethods;
             },
-            {} as BoundMethods<M>
+            {}
         ),
         [methods, setState]
     );
-    return [state, boundMethods];
+    return [state, boundMethods as M];
 }

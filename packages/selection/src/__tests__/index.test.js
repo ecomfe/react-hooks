16c13f36abdff8055e2acd4880ebd8994ffcb64b
otakustay <otakustay@gmail.com>
2020-02-24 16:02:58 +0800

    feat: selection hooks

---
+++ packages/selection/src/__tests__/index.test.js
@@ -0,0 +1,98 @@
+import {renderHook, act} from '@testing-library/react-hooks';
+import {useSelection} from '../index';
+
+test('default empty', () => {
+    const {result} = renderHook(() => useSelection());
+    expect(result.current[0]).toEqual([]);
+    expect(typeof result.current[1].selectIndex).toBe('function');
+});
+
+test('initial value', () => {
+    const {result} = renderHook(() => useSelection([1, 2, 3]));
+    expect(result.current[0]).toEqual([1, 2, 3]);
+});
+
+test('single select', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(1));
+    expect(result.current[0]).toEqual([1]);
+    act(() => result.current[1].selectIndex(2, {}));
+    expect(result.current[0]).toEqual([2]);
+});
+
+test('multiple select', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(1));
+    act(() => result.current[1].selectIndex(3, {metaKey: true}));
+    act(() => result.current[1].selectIndex(5, {ctrlKey: true}));
+    expect(result.current[0]).toEqual([1, 3, 5]);
+});
+
+test('remove select', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(1));
+    act(() => result.current[1].selectIndex(3, {metaKey: true}));
+    act(() => result.current[1].selectIndex(5, {ctrlKey: true}));
+    act(() => result.current[1].selectIndex(3, {ctrlKey: true}));
+    expect(result.current[0]).toEqual([1, 5]);
+});
+
+test('initial range', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(3, {shiftKey: true}));
+    expect(result.current[0]).toEqual([0, 1, 2, 3]);
+});
+
+test('range select', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(1));
+    act(() => result.current[1].selectIndex(3, {shiftKey: true}));
+    expect(result.current[0]).toEqual([1, 2, 3]);
+});
+
+test('reverse range', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(3));
+    act(() => result.current[1].selectIndex(1, {shiftKey: true}));
+    expect(result.current[0]).toEqual([3, 1, 2]);
+});
+
+test('range cover selected', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(1));
+    act(() => result.current[1].selectIndex(2, {ctrlKey: true}));
+    act(() => result.current[1].selectIndex(3, {shiftKey: true}));
+    expect(result.current[0]).toEqual([1, 2, 3]);
+});
+
+test('expand range', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(1));
+    act(() => result.current[1].selectIndex(3, {shiftKey: true}));
+    act(() => result.current[1].selectIndex(5, {shiftKey: true}));
+    expect(result.current[0]).toEqual([1, 2, 3, 4, 5]);
+});
+
+test('shrink range', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(1));
+    act(() => result.current[1].selectIndex(5, {shiftKey: true}));
+    act(() => result.current[1].selectIndex(3, {shiftKey: true}));
+    expect(result.current[0]).toEqual([1, 2, 3]);
+});
+
+test('reverse range', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(3));
+    act(() => result.current[1].selectIndex(5, {shiftKey: true}));
+    act(() => result.current[1].selectIndex(1, {shiftKey: true}));
+    expect(result.current[0]).toEqual([1, 2, 3]);
+});
+
+test('same range', () => {
+    const {result} = renderHook(() => useSelection());
+    act(() => result.current[1].selectIndex(1));
+    act(() => result.current[1].selectIndex(3, {shiftKey: true}));
+    act(() => result.current[1].selectIndex(3, {shiftKey: true}));
+    expect(result.current[0]).toEqual([1, 2, 3]);
+});
---
+++ packages/selection/src/index.ts
@@ -0,0 +1,102 @@
+import {useReducer, useCallback} from 'react';
+import union from 'lodash/union';
+import without from 'lodash/without';
+import difference from 'lodash/difference';
+import range from 'lodash/range';
+
+interface Action {
+    type: 'single' | 'multiple' | 'range';
+    payload: number;
+}
+
+export interface SelectionMethods {
+    selectIndex(index: number, e?: ClickContext): void;
+}
+
+type SelectionHook = [number[], SelectionMethods];
+
+interface ClickContext {
+    ctrlKey: boolean;
+    metaKey: boolean;
+    shiftKey: boolean;
+}
+
+interface SelectionContext {
+    rangeStart: number;
+    rangeEnd?: number;
+    selected: number[];
+}
+
+const getActionType = (e?: ClickContext): Action['type'] => {
+    if (!e) {
+        return 'single';
+    }
+    if (e.shiftKey) {
+        return 'range';
+    }
+    if (e.metaKey || e.ctrlKey) {
+        return 'multiple';
+    }
+    return 'single';
+};
+
+const toggleSingleSelected = (selected: number[], target: number): number[] => {
+    if (selected.includes(target)) {
+        return without(selected, target);
+    }
+    return selected.concat(target);
+};
+
+export function useSelection(initialSelection: number[] = []): SelectionHook {
+    const [{selected}, dispatch] = useReducer(
+        (state: SelectionContext, action: Action) => {
+            const {rangeStart, rangeEnd, selected} = state;
+            const {type, payload} = action;
+
+            if (type === 'range') {
+                if (rangeEnd === undefined) {
+                    const extra = payload < rangeStart
+                        ? range(payload, rangeStart + 1)
+                        : range(rangeStart, payload + 1);
+                    return {
+                        rangeStart,
+                        rangeEnd: payload,
+                        selected: union(selected, extra),
+                    };
+                }
+                else if (payload === rangeEnd) {
+                    return state;
+                }
+                else {
+                    const currentRange = range(
+                        Math.min(rangeStart, rangeEnd),
+                        Math.max(rangeStart, rangeEnd) + 1
+                    );
+                    const nextRange = range(
+                        Math.min(rangeStart, payload),
+                        Math.max(rangeStart, payload) + 1
+                    );
+                    const nextSelected = union(difference(selected, currentRange), nextRange);
+                    return {
+                        rangeStart,
+                        rangeEnd: payload,
+                        selected: nextSelected,
+                    };
+                }
+            }
+
+            const nextSelected = type === 'single' ? [payload] : toggleSingleSelected(selected, payload);
+            return {
+                rangeStart: payload,
+                rangeEnd: undefined,
+                selected: nextSelected,
+            };
+        },
+        {rangeStart: 0, rangeEnd: undefined, selected: initialSelection}
+    );
+    const selectIndex: SelectionMethods['selectIndex'] = useCallback(
+        (index, e) => dispatch({type: getActionType(e), payload: index}),
+        []
+    );
+    return [selected, {selectIndex}];
+}

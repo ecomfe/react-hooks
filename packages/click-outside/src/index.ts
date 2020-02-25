d39db770dd4d831c54c8526e5e0b92d7a12344ea
otakustay <otakustay@gmail.com>
2020-02-25 17:40:53 +0800

    feat: pass event object to useClickOutside callback

--- packages/click-outside/src/index.ts
+++ packages/click-outside/src/index.ts
@@ -1,10 +1,10 @@
 import {RefObject} from 'react';
 import {useDocumentEvent} from '@huse/document-event';
 
-export function useClickOutside(ref: RefObject<HTMLElement>, callback: () => void) {
+export function useClickOutside(ref: RefObject<HTMLElement>, callback: (e: MouseEvent | TouchEvent) => void) {
     const testAndTrigger = (e: MouseEvent | TouchEvent) => {
         if (!ref.current?.contains(e.target as Element)) {
-            callback();
+            callback(e);
         }
     };
 

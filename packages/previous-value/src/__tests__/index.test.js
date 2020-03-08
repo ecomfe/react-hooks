faf51e46f474dfb2dcadf26398c882ff094c9158
otakustay <otakustay@gmail.com>
2020-03-08 20:25:22 +0800

    test: remove react runtime dependency for hooks that can be tested via hooks testing library

--- packages/previous-value/src/__tests__/index.test.js
+++ packages/previous-value/src/__tests__/index.test.js
@@ -1,135 +1,84 @@
-/* eslint-disable react/jsx-no-bind */
-import {useState, useReducer} from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import {renderHook} from '@testing-library/react-hooks';
 import {usePreviousValue, usePreviousEquals, useOriginalCopy, useOriginalDeepCopy} from '../index';
 
-const useForceUpdate = () => useReducer(i => i + 1)[1];
-
 describe('usePreviousValue', () => {
-    const Foo = () => {
-        const [value, setValue] = useState(1);
-        const previousValue = usePreviousValue(value);
-
-        return (
-            <>
-                <span id="previous">{previousValue || 'undefined'}</span>
-                <span id="current">{value}</span>
-                <span id="inc" onClick={() => setValue(i => i + 1)}>Increment</span>
-            </>
-        );
-    };
-
     test('initial undefined', () => {
-        const {container} = render(<Foo />);
-        expect(container.querySelector('#previous').textContent).toBe('undefined');
-        expect(container.querySelector('#current').textContent).toBe('1');
+        const {result} = renderHook(() => usePreviousValue(1));
+        expect(result.current).toBe(undefined);
     });
 
     test('cache previous', () => {
-        const {container} = render(<Foo />);
-        fireEvent.click(container.querySelector('#inc'));
-        expect(container.querySelector('#previous').textContent).toBe('1');
-        expect(container.querySelector('#current').textContent).toBe('2');
+        const {result, rerender} = renderHook(props => usePreviousValue(props.value), {initialProps: {value: 1}});
+        rerender({value: 2});
+        expect(result.current).toBe(1);
     });
 });
 
 describe('usePreviousEquals', () => {
-    const Foo = ({customEquals}) => {
-        const [value, setValue] = useState(1);
-        const update = useForceUpdate();
-        const equals = usePreviousEquals(value, customEquals);
-
-        return (
-            <>
-                <span id="equals">{equals.toString()}</span>
-                <span id="inc" onClick={() => setValue(i => i + 1)}>Increment</span>
-                <span id="update" onClick={update}>Increment</span>
-            </>
-        );
-    };
-
     test('initial false', () => {
-        const {container} = render(<Foo />);
-        expect(container.querySelector('#equals').textContent).toBe('false');
+        const {result} = renderHook(() => usePreviousEquals(1));
+        expect(result.current).toBe(false);
     });
 
     test('false after change', () => {
-        const {container} = render(<Foo />);
-        fireEvent.click(container.querySelector('#inc'));
-        expect(container.querySelector('#equals').textContent).toBe('false');
+        const {result, rerender} = renderHook(props => usePreviousEquals(props.value), {initialProps: {value: 1}});
+        rerender({value: 2});
+        expect(result.current).toBe(false);
     });
 
     test('true when unchanged', () => {
-        const {container} = render(<Foo />);
-        fireEvent.click(container.querySelector('#update'));
-        expect(container.querySelector('#equals').textContent).toBe('true');
+        const {result, rerender} = renderHook(props => usePreviousEquals(props.value), {initialProps: {value: 1}});
+        rerender({value: 1});
+        expect(result.current).toBe(true);
     });
 
     test('custom equals', () => {
-        const {container} = render(<Foo customEquals={() => true} />);
-        fireEvent.click(container.querySelector('#inc'));
-        expect(container.querySelector('#equals').textContent).toBe('true');
+        const {result, rerender} = renderHook(
+            props => usePreviousEquals(props.value, () => true),
+            {initialProps: {value: 1}}
+        );
+        rerender({value: 2});
+        expect(result.current).toBe(true);
     });
 });
 
 describe('useOriginalCopy', () => {
-    const Foo = ({customEquals}) => {
-        const [value, setValue] = useState({foo: 1});
-        const original = useOriginalCopy(value, customEquals);
-        const previous = usePreviousValue(value);
-
-        return (
-            <>
-                <span id="value">{original.foo}</span>
-                <span id="equals">{(original === previous).toString()}</span>
-                <span id="inc" onClick={() => setValue(i => ({foo: i.foo + 1}))}>Increment</span>
-                <span id="same" onClick={() => setValue(i => ({foo: i.foo}))}>Increment</span>
-            </>
-        );
-    };
-
     test('initial value', () => {
-        const {container} = render(<Foo />);
-        expect(container.querySelector('#value').textContent).toBe('1');
+        const props = {value: 1};
+        const {result} = renderHook(() => useOriginalCopy(props));
+        expect(result.current).toBe(props);
     });
 
     test('keep change', () => {
-        const {container} = render(<Foo />);
-        fireEvent.click(container.querySelector('#inc'));
-        expect(container.querySelector('#value').textContent).toBe('2');
+        const {result, rerender} = renderHook(props => useOriginalCopy(props), {initialProps: {value: 1}});
+        const props = {value: 2};
+        rerender(props);
+        expect(result.current).toBe(props);
     });
 
     test('original reference when content equals', () => {
-        const {container} = render(<Foo />);
-        fireEvent.click(container.querySelector('#same'));
-        expect(container.querySelector('#equals').textContent).toBe('true');
+        const props = {value: 1};
+        const {result, rerender} = renderHook(props => useOriginalCopy(props), {initialProps: props});
+        rerender({value: 1});
+        expect(result.current).toBe(props);
     });
 
     test('custom equals', () => {
-        const {container} = render(<Foo customEquals={() => false} />);
-        fireEvent.click(container.querySelector('#same'));
-        expect(container.querySelector('#equals').textContent).toBe('false');
+        const {result, rerender} = renderHook(
+            props => useOriginalCopy(props, () => false),
+            {initialProps: {value: 1}}
+        );
+        const props = {value: 1};
+        rerender(props);
+        expect(result.current).toBe(props);
     });
 });
 
 describe('useOriginalDeepCopy', () => {
-    const Foo = () => {
-        const [value, setValue] = useState({foo: {bar: 1}});
-        const original = useOriginalDeepCopy(value);
-        const previous = usePreviousValue(value);
-
-        return (
-            <>
-                <span id="value">{original.foo.bar}</span>
-                <span id="equals">{(original === previous).toString()}</span>
-                <span id="same" onClick={() => setValue(i => ({foo: {bar: i.foo.bar}}))}>Increment</span>
-            </>
-        );
-    };
-
     test('original reference when deep equal', () => {
-        const {container} = render(<Foo customEquals={() => false} />);
-        fireEvent.click(container.querySelector('#same'));
-        expect(container.querySelector('#equals').textContent).toBe('true');
+        const props = {value: {foo: 1}};
+        const {result, rerender} = renderHook(props => useOriginalDeepCopy(props), {initialProps: props});
+        rerender({value: {foo: 1}});
+        expect(result.current).toBe(props);
     });
 });
--- packages/update/src/__tests__/index.test.js
+++ packages/update/src/__tests__/index.test.js
@@ -1,6 +1,4 @@
-import {useRef} from 'react';
-import {render, fireEvent} from '@testing-library/react';
-import {renderHook} from '@testing-library/react-hooks';
+import {renderHook, act} from '@testing-library/react-hooks';
 import {useForceUpdate} from '../index';
 
 test('valid return type', () => {
@@ -9,16 +7,9 @@ test('valid return type', () => {
 });
 
 test('will force update', () => {
-    const Foo = () => {
-        const times = useRef(0);
-        times.current++;
-        const forceUpdate = useForceUpdate();
-
-        return <div onClick={forceUpdate}>{times.current}</div>;
-    };
-
-    const {container} = render(<Foo />);
-    const root = container.querySelector('div');
-    fireEvent.click(root);
-    expect(root.textContent).toBe('2');
+    const init = jest.fn(() => useForceUpdate());
+    const {result} = renderHook(init);
+    expect(init).toHaveBeenCalledTimes(1);
+    act(() => result.current());
+    expect(init).toHaveBeenCalledTimes(2);
 });

3e286118754c71a7f04d47a6e02dfa6f7817e595
otakustay <otakustay@gmail.com>
2020-03-09 12:19:23 +0800

    refractor: implement useSet and useMap on the new useMethodsNative hook

--- packages/collection/src/map.ts
+++ packages/collection/src/map.ts
@@ -1,70 +1,42 @@
-import {useState, useMemo, Dispatch, SetStateAction} from 'react';
-
-export interface MapMethods<K, V> {
-    set(key: K, value: V): void;
-    setAll(entries: Iterable<[K, V]>): void;
-    delete(key: K): void;
-    deleteAll(keys: Iterable<K>): void;
-    clear(): void;
-}
-
-export type MapHook<K, V> = [Map<K, V>, MapMethods<K, V>, Dispatch<SetStateAction<Map<K, V>>>];
+import {useMethodsNative} from '@huse/methods';
 
 const clone = <K, V>(map: Map<K, V>) => new Map(map.entries());
 
-const reducers = {
-    set<K, V>(state: Map<K, V>, key: K, value: V) {
-        return clone(state).set(key, value);
-    },
-    setAll<K, V>(state: Map<K, V>, entries: Iterable<[K, V]>) {
-        const output = clone(state);
-        for (const [key, value] of entries) {
-            output.set(key, value);
-        }
-        return output;
-    },
-    delete<K, V>(state: Map<K, V>, key: K) {
-        if (state.has(key)) {
-            const output = clone(state);
-            output.delete(key);
-            return output;
-        }
-
-        return state;
-    },
-    deleteAll<K, V>(state: Map<K, V>, keys: Iterable<K>) {
-        const output = clone(state);
-        for (const key of keys) {
-            output.delete(key);
-        }
-        return output;
-    },
-};
-
-export default function useMap<K, V>(initialValue: Iterable<[K, V]> = []): MapHook<K, V> {
-    const [value, setValue] = useState(() => new Map(initialValue));
-    const methods = useMemo(
-        () => {
-            return {
-                set(key: K, value: V) {
-                    setValue(state => reducers.set(state, key, value));
-                },
-                setAll(entries: Iterable<[K, V]>) {
-                    setValue(state => reducers.setAll(state, entries));
-                },
-                delete(key: K) {
-                    setValue(state => reducers.delete(state, key));
-                },
-                deleteAll(keys: Iterable<K>) {
-                    setValue(state => reducers.deleteAll(state, keys));
-                },
-                clear() {
-                    setValue(new Map<K, V>());
-                },
-            };
+function createReducers<K, V>() {
+    return {
+        set(state: Map<K, V>, key: K, value: V) {
+            return clone(state).set(key, value);
         },
-        []
-    );
-    return [value, methods, setValue];
+        setAll(state: Map<K, V>, entries: Iterable<[K, V]>) {
+            const output = clone(state);
+            for (const [key, value] of entries) {
+                output.set(key, value);
+            }
+            return output;
+        },
+        delete(state: Map<K, V>, key: K) {
+            if (state.has(key)) {
+                const output = clone(state);
+                output.delete(key);
+                return output;
+            }
+
+            return state;
+        },
+        deleteAll(state: Map<K, V>, keys: Iterable<K>) {
+            const output = clone(state);
+            for (const key of keys) {
+                output.delete(key);
+            }
+            return output;
+        },
+        clear() {
+            return new Map<K, V>();
+        },
+    };
+}
+
+export default function useMap<K, V>(initialValue: Iterable<[K, V]> = []) {
+    return useMethodsNative(createReducers<K, V>(), () => new Map(initialValue));
 }
 
--- packages/collection/src/set.ts
+++ packages/collection/src/set.ts
@@ -1,74 +1,46 @@
-import {useState, useMemo, SetStateAction, Dispatch} from 'react';
-
-export interface SetMethods<T> {
-    add(item: T): void;
-    addAll(items: Iterable<T>): void;
-    delete(item: T): void;
-    deleteAll(items: Iterable<T>): void;
-    clear(): void;
-}
-
-export type SetHook<T> = [Set<T>, SetMethods<T>, Dispatch<SetStateAction<Set<T>>>];
+import {useMethodsNative} from '@huse/methods';
 
 const clone = <T>(set: Set<T>) => new Set(set.values());
 
-const reducers = {
-    add<T>(state: Set<T>, item: T) {
-        if (state.has(item)) {
-            return state;
-        }
+function createReducers<T>() {
+    return {
+        add(state: Set<T>, item: T) {
+            if (state.has(item)) {
+                return state;
+            }
 
-        return clone(state).add(item);
-    },
-    addAll<T>(state: Set<T>, items: Iterable<T>) {
-        const output = clone(state);
-        for (const item of items) {
-            output.add(item);
-        }
-        return output;
-    },
-    delete<T>(state: Set<T>, item: T) {
-        if (state.has(item)) {
-            const output = clone(state);
-            output.delete(item);
-            return output;
-        }
-
-        return state;
-    },
-    deleteAll<T>(state: Set<T>, items: Iterable<T>) {
-        const output = clone(state);
-        for (const item of items) {
-            output.delete(item);
-        }
-        return output;
-    },
-};
-
-export default function useSet<T>(initialValue: Iterable<T> = []): SetHook<T> {
-    const [value, setValue] = useState(() => new Set(initialValue));
-    const methods = useMemo(
-        () => {
-            return {
-                add(item: T) {
-                    return setValue(state => reducers.add(state, item));
-                },
-                addAll(items: Iterable<T>) {
-                    return setValue(state => reducers.addAll(state, items));
-                },
-                delete(item: T) {
-                    return setValue(state => reducers.delete(state, item));
-                },
-                deleteAll(items: Iterable<T>) {
-                    return setValue(state => reducers.deleteAll(state, items));
-                },
-                clear() {
-                    setValue(new Set<T>());
-                },
-            };
+            return clone(state).add(item);
         },
-        []
-    );
-    return [value, methods, setValue];
+        addAll(state: Set<T>, items: Iterable<T>) {
+            const output = clone(state);
+            for (const item of items) {
+                output.add(item);
+            }
+            return output;
+        },
+        delete(state: Set<T>, item: T) {
+            if (state.has(item)) {
+                const output = clone(state);
+                output.delete(item);
+                return output;
+            }
+
+            return state;
+        },
+        deleteAll(state: Set<T>, items: Iterable<T>) {
+            const output = clone(state);
+            for (const item of items) {
+                output.delete(item);
+            }
+            return output;
+        },
+        clear() {
+            return new Set<T>();
+        },
+    };
+}
+
+export default function useSet<T>(initialValue: Iterable<T> = []) {
+    return useMethodsNative(createReducers<T>(), () => new Set(initialValue));
 }
 

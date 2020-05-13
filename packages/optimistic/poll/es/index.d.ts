22647ef02a3a369c004ed97b30afd36ab7edf69d
zhangzhe <zhangzhe@baidu.com>
2020-05-13 11:45:52 +0800

    poll位置放对

--- packages/optimistic/poll/es/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface PollOptions {
-    minInterval: number;
-    maxInterval?: number;
-    maxIdleTime?: number;
-    stopOnInactive?: boolean;
-}
-export declare type PollHook<S> = [S | undefined, number];
-export declare function usePoll<S>(fetch: () => Promise<S>, options: number | PollOptions): PollHook<S>;

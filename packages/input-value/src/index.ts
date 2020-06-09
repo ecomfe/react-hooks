8d37350e351ec0115a143b1f80f1c359e7a97b34
otakustay <otakustay@gmail.com>
2020-06-09 11:21:32 +0800

    fix(input-value): make useInputValue type compatible to textarea

--- packages/input-value/src/index.ts
+++ packages/input-value/src/index.ts
@@ -1,14 +1,18 @@
-import {useState, useCallback, ChangeEvent} from 'react';
+import {useState, useCallback} from 'react';
+
+interface ChangeEvent {
+    target: {value: string};
+}
 
 export interface InputValueState {
     value: string;
-    onChange(e: ChangeEvent<HTMLInputElement>): void;
+    onChange(e: ChangeEvent): void;
 }
 
 export function useInputValue(initialValue: string = ''): InputValueState {
     const [value, setValue] = useState(initialValue);
     const onChange = useCallback(
-        (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value),
+        (e: ChangeEvent) => setValue(e.target.value),
         []
     );
     return {value, onChange};

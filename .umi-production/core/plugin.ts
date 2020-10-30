d490b4c4d55eaa8c654796626a58c5216624bfbb
yuxiaoyang03 <yuxiaoyang03@baidu.com>
2020-10-30 17:33:49 +0800

    delete .umi-production/

--- .umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/yuxiaoyang03/Desktop/react-hooks/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
--- .umi-production/core/pluginRegister.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-import { plugin } from './plugin';
-

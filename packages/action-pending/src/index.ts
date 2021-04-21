cf3644f996271da2369e6db07a123c38a16d7a06
SyMind <dacongsama@live.com>
2021-04-21 08:03:38 +0800

    fix(action-pending): prevent state update in unmounted component

--- packages/action-pending/src/index.ts
+++ packages/action-pending/src/index.ts
@@ -1,18 +1,27 @@
-import {useCallback} from 'react';
+import {useCallback, useEffect, useRef} from 'react';
 import {useCounter} from '@huse/number';
 
 type AsyncFunction = (...args: any[]) => Promise<any>;
 
 export function useActionPending<A extends AsyncFunction>(action: A): [A, number] {
+    const unmounted = useRef(false);
     const [pendingCount, {inc, dec}] = useCounter();
     const actionWithPending = useCallback(
         (...args: Parameters<A>) => {
             inc();
             const pending = action(...args);
-            pending.then(dec, dec);
+            const safeDec = () => {
+                if (!unmounted.current) {
+                    dec();
+                }
+            };
+            pending.then(safeDec, safeDec);
             return pending;
         },
         [action, dec, inc]
     );
+    useEffect(() => () => {
+        unmounted.current = true;
+    }, []);
     return [actionWithPending as A, pendingCount];
 }
